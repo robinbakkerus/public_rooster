9aa7c29cd7320af7a32cf9a070d6a86997ce5945
robin.bakkerus <robin.bakkerus@gmail.com>
2025-01-11 13:44:42 +0100

    fix dates

--- lib/util/app_helper.dart
+++ lib/util/app_helper.dart
@@ -73,7 +73,7 @@ class AppHelper {
   List<DateTime> getDaysInBetween(DateTime startDate) {
     DateTime endDate = DateTime(startDate.year, startDate.month + 1, 1);
     List<DateTime> days = [];
-    for (int i = 0; i <= endDate.difference(startDate).inDays; i++) {
+    for (int i = 0; i <= endDate.difference(startDate).inDays - 1; i++) {
       days.add(startDate.add(Duration(days: i)));
     }
     return days;
